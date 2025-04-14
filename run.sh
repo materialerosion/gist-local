c7c9898faee88af71e6d12c15119dc806bb41591
materialerosion <141874499+materialerosion@users.noreply.github.com>
2025-04-14 11:07:53 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
-python -m streamlit run ui/app.py \ --server.port 8000 \ --server.address 0.0.0.0 \ --server.enableCORS false \ --server.enableXsrfProtection false \ --server.enableWebsocketCompression false \ --server.enableWebsocketConnectionReuse false \ --server.headless true \ --browser.serverAddress "https://gist-b.azurewebsites.net" \ --server.runOnSave false \ --client.showErrorDetails false \ --client.toolbarMode minimal \ --server.enableStaticServing true
+python -m streamlit run streamlit_app.py \ --server.port 8000 \ --server.address 0.0.0.0 \ --server.enableCORS false \ --server.enableXsrfProtection false \ --server.enableWebsocketCompression false \ --server.enableWebsocketConnectionReuse false \ --server.headless true \ --browser.serverAddress "https://gist-b.azurewebsites.net" \ --server.runOnSave false \ --client.showErrorDetails false \ --client.toolbarMode minimal \ --server.enableStaticServing true
 
 
